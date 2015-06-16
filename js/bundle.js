d48407960fd5f8959a4ad9e8eb9e082b19f3895d
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2015-06-17 00:59:30 +0800

    publish: Add multiple sample support
    generated from commit 03146a464c940093fc4f59a98d786ef10d22c0e9

--- js/bundle.js
+++ js/bundle.js
@@ -55,15 +55,46 @@
 	  [].forEach.call(docsRepl, function(elm) {
 	    var textarea = elm.getElementsByTagName('textarea')[0];
 	    var iframe = elm.getElementsByTagName('iframe')[0];
-	    
+	    var select = elm.getElementsByTagName('select')[0];
+
+	    // init codemirror
 	    var cm = CodeMirror.fromTextArea(textarea, {
 	      lineNumbers: true,
 	      mode: "javascript"
 	    });
+
+	    var code_sample = processCode(cm, select);
+
+	    // attach event listeners
+	    if(select) {
+	      select.onchange = function() {
+	        var text = this.children[this.selectedIndex].value.trim();
+	        cm.setValue(text);
+	      };
+	    }
 	    cm.on("change", function(cm, change) {
 	      iframe.src = "http://overpass-turbo.eu/map.html?Q=" + escape(cm.getValue());
 	    });
+
+	    // set codemirror to default text
+	    cm.setValue(code_sample[1]);
 	  });
+	  function processCode(cm, select) {
+	    var code_sample = cm.getDoc().getValue().split(/===/);
+	    if (code_sample.length === 0) {
+	      code_sample = ['', cm.getDoc().getValue()];
+	    } else {
+	      code_sample.shift(); //remove first element (since split creates empty first value)
+	      code_sample = code_sample.map(Function.prototype.call, String.prototype.trim); // apply trim
+	      for(var i = 0; i < code_sample.length; i += 2) {
+	        var opt = document.createElement('option');
+	        opt.value = code_sample[i+1];
+	        opt.innerHTML = code_sample[i];
+	        select.appendChild(opt);
+	      }
+	    }
+	    return code_sample;
+	  }
 	})();
 
 
